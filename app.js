0585a842b8e31f7d1a2076d69b1652b1d3a370b8
Eyu-Gen <dreamersthebig@gmail.com>
2025-06-18 22:54:58 +0545

    Incomplete

--- app.js
+++ app.js
@@ -9,12 +9,93 @@ function removeAndAddClass(element, toggleVar) {
     }
 }
 
+// Function to create new comment container...
+function createCommentHTML(commentText) {
+    return `
+        <div class="container commentContainer">
+            <!-- Comment Author Details -->
+            <div class="header center">
+                <div class="subContainer center commentAuthorContainer authorContainer">
+                    <div class="imageSection center authorImageContainer commentAuthorImageContainer">
+                        <img src="Images/1.png" alt="" class="commentAuthorImage">
+                    </div>
+                    <div class="authorDetailContainer commentAuthorDetailContainer">
+                        <p class="authorName commentAuthorName commentTo">You</p>
+                        <p class="subText">Just Now</p>
+                    </div>
+                </div>
+                <div class="settingContainer center">
+                    <div class="settingOption center" id="editBTN">
+                        <div class="imageSection center settingIcon">
+                            <img src="Images/icon-edit.svg" alt="Edit" id="editIcon">
+                        </div>
+                        <span class="iconName plainText" id="editText">Edit</span>
+                    </div>
+                    <div class="settingOption center deleteBTN" id="">
+                        <div class="imageSection center settingIcon">
+                            <img src="Images/icon-delete.svg" alt="Delete" id="deleteIcon">
+                        </div>
+                        <span class="iconName plainText" id="deleteText">Delete</span>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Comment Content -->
+            <div class="subContainer commentContent">
+                <span class="highlight">Yugin Paudel</span> ${commentText}
+            </div>
+            <hr>
+
+            <!-- Comment Engagement -->
+            <div class="subContainer center engagementContainer commentEngagementContainer">
+                <div class="interactContainer center commentReplyBtn">
+                    <p class="subText replyBtn">Reply</p>
+                </div>
+                <div class="interactContainer center viewReplyBTN">
+                    <p class="subText viewReplyTxt">View Reply (<span class="replyCounter">0</span>)</p>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+// Function for Reply Container Show and Hide... [ERROE HERE]
+function replyContainerView() {
+    const replyBtn = document.querySelectorAll(".viewReplyBTN");
+    const viewReplyTxt = document.querySelectorAll(".viewReplyTxt");
+    const replyContainer = document.querySelectorAll(".replyContainer");
+    const replyCounters = document.getElementsByClassName("replyCounter");
+    Array.from(replyCounters).forEach((replyCounter) => {
+        let replyCount = parseInt(replyCounter.innerHTML);
+        Array.from(replyBtn).forEach((btn) => {
+            let isClicked = false;
+            btn.addEventListener("click", () => {
+                console.log(replyCount);
+                if(replyCount==0){
+                    alert("No Reply To Show!!");
+                }
+                else{
+                    const replyIndex = Array.from(replyBtn).indexOf(btn);
+                    isClicked = !isClicked;
+                    if (isClicked) {
+                        removeAndAddClass(replyContainer[replyIndex], 1);
+                        viewReplyTxt[replyIndex].innerHTML = "Hide Reply";
+                    }
+                    else {
+                        removeAndAddClass(replyContainer[replyIndex], 0);
+                        viewReplyTxt[replyIndex].innerHTML = `View Reply (${replyCount})`;
+                    }
+                }
+            });
+        });
+    });
+}
 
 //Like Increment................
 const likeBtn = document.getElementById("likeBTN");
-const likeCounter = document.getElementById("likeCounter");
 const unclickedLikeIcon = document.getElementById("unclickedLikeIcon");
 const clickedLikeIcon = document.getElementById("clickedLikeIcon");
+const likeCounter = document.getElementById("likeCounter");
 let likeCount = parseInt(likeCounter.innerHTML);
 let isliked = false;
 likeBtn.addEventListener("click", () => {
@@ -31,6 +112,9 @@ likeBtn.addEventListener("click", () => {
         removeAndAddClass(clickedLikeIcon, 0);
     }
 });
+
+const commentCounter = document.getElementById("commentCounter");
+let commentCount = parseInt(commentCounter.innerHTML);
 
 // Comment Section Show/Hide................
 const showAllCommentBTN = document.getElementById("allCommentBtn");
@@ -49,38 +133,23 @@ showAllCommentBTN.addEventListener("click", () => {
     }
 });
 
-//Reply container show/hide......
-const replyBtn = document.querySelectorAll(".viewReplyBTN");
-const viewReplyTxt = document.querySelectorAll(".viewReplyTxt");
-const replyContainer = document.querySelectorAll(".replyContainer");
-Array.from(replyBtn).forEach((btn) => {
-    let isClicked = false;
-    btn.addEventListener("click", () => {
-        const replyIndex = Array.from(replyBtn).indexOf(btn);
-        isClicked = !isClicked;
-        if (isClicked) {
-            removeAndAddClass(replyContainer[replyBtn], 1);
-            viewReplyTxt[replyIndex].innerHTML = "Hide Reply";
-        }
-        else {
-            removeAndAddClass(replyContainer[replyIndex], 0);
-            viewReplyTxt[replyIndex].innerHTML = "View Reply (1)";
-        }
-    });
-});
+//Reply container show/hide......[FUNCTION TESTING]
+replyContainerView();
 
 // Delete Reply confirmation container popup while clicking on delete btn.....
 const alertContainerWrapper = document.querySelectorAll(".alertContainerWrapper");
-const deleteBTN = document.getElementById("deleteBTN");
+const deleteBTNs = document.getElementsByClassName("deleteBTN");
 const commentContainer = document.getElementById("comment2Container");
 const deleteConfirmContainer = document.getElementById("deleteConfirmContainer");
-deleteBTN.addEventListener("click", () => {
-    Array.from(alertContainerWrapper).forEach((bg) => {
-        removeAndAddClass(bg, 1);
-    });
-    removeAndAddClass(deleteConfirmContainer, 1);
-    removeAndAddClass(commentTypingContainer, 0);
-});
+Array.from(deleteBTNs).forEach((deleteBTN) => {
+        deleteBTN.addEventListener("click", () => {
+        Array.from(alertContainerWrapper).forEach((bg) => {
+            removeAndAddClass(bg, 1);
+        });
+        removeAndAddClass(deleteConfirmContainer, 1);
+        removeAndAddClass(commentTypingContainer, 0);
+    }
+)});
 
 //Cancel Deletion of reply comment popup while clicking on cancel btn.....
 const cancelDeleteBTN = document.getElementById("cancelDeleteBTN");
@@ -121,6 +190,8 @@ cancelPostBTN.addEventListener("click", () => {
         removeAndAddClass(bg, 0);
     });
     removeAndAddClass(commentTypingContainer, 0);
+    (document.getElementById("errorContainer")).classList.add("visibilityHidden");
+    (document.getElementById("errorContainer")).classList.remove("visibilityVisible");
 });
 
 //Comment Typing container hide and show....
@@ -143,52 +214,20 @@ const postBTN = document.getElementById("postBTN");
 const newCommentSection = document.getElementById("newCommentSection");
 postBTN.addEventListener("click", () => {
     const commentTypingContent = (document.getElementById("commentTypingContent")).value;
-    
-    newCommentSection.innerHTML = `
-        <div class="container commentContainer">
-                 <!-- Comment Author Details -->
-                  <div class="header center">
-                      <div class="subContainer center commentAuthorContainer authorContainer">
-                          <div class="imageSection center authorImageContainer commentAuthorImageContainer">
-                              <img src="Images/1.png" alt="" class="commentAuthorImage">
-                          </div>
-          
-                          <div class="authorDetailContainer  commentAuthorDetailContainer">
-                              <p class="authorName commentAuthorName commentTo">You</p>
-                              <p class="subText">Just Now</p>
-                          </div>
-                      </div>
-                      <div class="settingContainer center" id="settingContainer"> <!--ERROR HERE-->
-                        <div class="settingOption center" id="editBTN">
-                            <div class="imageSection center settingIcon">
-                                <img src="Images/icon-edit.svg" alt="" id="editIcon"> 
-                            </div>
-                            <span class="iconName plainText" id="editText">Edit</span>
-                        </div>
-                        <div class="settingOption center" id="deleteBTN">
-                            <div class="imageSection center settingIcon">
-                                <img src="Images/icon-delete.svg" alt="" id="deleteIcon"> 
-                            </div>
-                            <span class="iconName plainText" id="deleteText">Delete</span>
-                        </div>
-                        </div>
-                    </div>
-     
-                 <!-- Comment Content -->
-                 <div class="subContainer commentContent" id="comment2Content">
-                     <span class="highlight">Yugin Paudel</span> ${commentTypingContent}
-                 </div>
-                 <hr>
-     
-                 <!-- Comment Engagement and Inverative Section-->
-                 <div class="subContainer center engagementContainer commentEngagementContainer" id="comment2EngagementContainer">
-                     <div class="interactContainer center commentReplyBtn">
-                         <p class="subText replyBtn">Reply</p>
-                     </div>
-                     <div class="interactContainer center viewReplyBTN">
-                         <p class="subText viewReplyTxt">View Reply (0)</p>
-                     </div>
-                 </div>
-             </div>
-    `;
+    if(commentTypingContent==""){
+        (document.getElementById("errorContainer")).classList.remove("visibilityHidden");
+        (document.getElementById("errorContainer")).classList.add("visibilityVisible");
+    }
+    else {
+        (document.getElementById("errorContainer")).classList.add("visibilityHidden");
+        (document.getElementById("errorContainer")).classList.remove("visibilityVisible");
+        newCommentSection.innerHTML += createCommentHTML(commentTypingContent);
+        Array.from(alertContainerWrapper).forEach((bg) => {
+            removeAndAddClass(bg, 0);
+        });
+        removeAndAddClass(commentTypingContainer, 0);
+        commentCount++;
+        commentCounter.innerText = `${commentCount}`;
+        replyContainerView();
+    }
 });
